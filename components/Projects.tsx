89a7e65414003cb6cf3fc3f76321d7b42d847ed1
Amogh Shetty <amoggo2004@gmail.com>
2024-12-18 18:05:30 +0300

    Fixed floating navbar

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,7 +5,7 @@ import { FaLocationArrow } from 'react-icons/fa6'
 
 const Projects = () => {
   return (
-    <div className="py-20">
+    <div className="py-20" id="projects">
         <h1 className='heading'>
             Here are some of my {' '}
             <span className='text-purple'>recent projects</span>
