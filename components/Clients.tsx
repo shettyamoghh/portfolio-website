9470a61091b978acd8758a73c565e1b4b6d15a57
Amogh Shetty <amoggo2004@gmail.com>
2024-12-18 21:37:55 +0300

    Fixed testimonials padding

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -9,13 +9,13 @@ const Clients = () => {
             Testimonials from {' '}
             <span className='text-purple'>satisfied clients</span>
         </h1>
-        <div className='flex flex-col items-center max-lg:mt-10'>
+        <div className='flex flex-col items-center max-lg:mt-10 mt-10 mb-10'>
             <InfiniteMovingCards
             items={testimonials}
             direction='right'
             speed='slow'
             />
-            <div className='flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10'>
+            <div className='flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10 mt-10'>
                 {companies.map(({id, img, name, nameImg}) => (
                     <div key={id} className='flex md:max-w-60 max-w-32 gap-2'>
                         <img 
