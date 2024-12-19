121112d07e58bed9882c24713d4139e7923b5708
Amogh Shetty <amoggo2004@gmail.com>
2024-12-19 17:29:22 +0300

    Bug fix - header and footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,13 +6,14 @@ import { socialMedia } from '@/data'
 const Footer = () => {
   return (
     <footer className='w-full pt-20 pb-10' id='contact'>
-        <div className='w-full absolute left-0 -bottom-72 min-h-96'>
+        {/* removed footer image - causing excess space at the bottom */}
+        {/* <div className='w-full absolute left-0 -bottom-72 min-h-96'>
             <img 
                 src="/footer-grid.svg"
                 alt="grid"
                 className='w-full h-full opacity-50'
             />
-        </div>
+        </div> */}
         <div className='flex flex-col items-center'>
             <h1 className='heading lg:max-w-[45vw]'>
                 Want to <span className='text-purple'>connect?</span>
