3db0af63f4cd6736d61b64d85478fe61ae96e986
Amogh Shetty <amoggo2004@gmail.com>
2025-01-01 14:31:26 +0300

    Update text in bento grid

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -109,7 +109,7 @@ export const BentoGridItem = ({
         {id === 3 && (
           <div className="flex gap-1 lg:gap-5 w-fit absolute -right-3 lg:-right-2 opacity-30 xl:opacity-100">
             <div className="flex flex-col gap-3 lg:gap-8">
-              {['ReactJS', 'NextJS', 'JavaScript'].map((item) => (
+              {['ReactJS', 'JavaScript', 'Node.js'].map((item) => (
                 <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
                   {item}
                 </span>
@@ -118,7 +118,7 @@ export const BentoGridItem = ({
             </div>
             <div className="flex flex-col gap-3 lg:gap-8">
             <span className="py-4 px-3 rounded-lg text-center bg-[#10132E]"/>
-              {['TypeScript', 'Flutter', 'Swift'].map((item) => (
+              {['Python', 'MySQL', 'Flutter'].map((item) => (
                 <span key={item} className="py-2 lg:py-4 lg:px-3 px-3 text-xs lg:text-base opacity-50 lg:opacity-100 rounded-lg text-center bg-[#10132E]">
                   {item}
                 </span>
