cd017b79769b275a78007c683766704284df500d
Amogh Shetty <amoggo2004@gmail.com>
2025-03-18 13:17:55 -0700

    Updated Bento Grid

--- data/index.ts
+++ data/index.ts
@@ -1,213 +1,216 @@
 export const navItems = [
-    { name: "About", link: "#about" },
-    { name: "Projects", link: "#projects" },
-    { name: "Experience", link: "#experience" },
-    { name: "Contact", link: "#contact" },
-  ];
-  
-  export const gridItems = [
-    {
-      id: 1,
-      title: "Currently a Junior Computer Science student at Arizona State University",
-      description: "",
-      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
-      imgClassName: "w-full h-full",
-      titleClassName: "justify-end",
-      img: "/b1.png",
-      spareImg: "",
-    },
-    {
-      id: 2,
-      title: "Seamless communication across various time zones",
-      description: "",
-      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
-      imgClassName: "",
-      titleClassName: "justify-start",
-      img: "",
-      spareImg: "",
-    },
-    {
-      id: 3,
-      title: "making use of",
-      description: "Here is what I am currently",
-      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
-      imgClassName: "",
-      titleClassName: "justify-center",
-      img: "",
-      spareImg: "",
-    },
-    {
-      id: 4,
-      title: "Passionate about tech, personal finance, and productivity",
-      description: "",
-      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
-      imgClassName: "",
-      titleClassName: "justify-start",
-      img: "/grid.svg",
-      spareImg: "/b4.svg",
-    },
-    {
-      id: 5,
-      title: "Learning how to integrate artificial intelligence into web applications",
-      description: "January 2025",
-      className: "md:col-span-3 md:row-span-2",
-      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60 opacity-20 xl:opacity-70",
-      titleClassName: "justify-center md:justify-start lg:justify-center",
-      img: "/b5.svg",
-      spareImg: "/grid.svg",
-    },
-    {
-      id: 6,
-      title: "Do you want to start a project together?",
-      description: "",
-      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
-      imgClassName: "",
-      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
-      img: "",
-      spareImg: "",
-    },
-  ];
-  
-  export const projects = [
-    {
-      id: 1,
-      title: "Let Him Cook AI",
-      des: "React web-application that generates delicious recipes based on your ingredients, powered by Claude's API.",
-      img: "/p2.png",
-      iconLists: ["/re.svg", "/claude.svg", "/html.svg", "/css.svg", "/js.svg"],
-      link: "https://lethimcook-ai.netlify.app/",
-    },
-    {
-      id: 2,
-      title: "Tenzies Game",
-      des: "Responsive dice game built with React that's both responsive and fun!",
-      img: "/p3.png",
-      iconLists: ["/re.svg", "/html.svg", "/css.svg", "/js.svg"],
-      link: "https://tenzies-shetty.netlify.app/",
-    },
-    {
-      id: 3,
-      title: "CRUD To-Do List",
-      des: "Keep track of your tasks using this simple to-do list made with React. The tasks never end huh?",
-      img: "/p1.png",
-      iconLists: ["/re.svg", "/html.svg", "/css.svg", "/js.svg"],
-      link: "https://shetty-todolist.netlify.app/",
-    },
-    // {
-    //   id: 4,
-    //   title: "title",
-    //   des: "description",
-    //   img: "/p4.svg",
-    //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    //   link: "https://github.com/shettyamoghh",
-    // },
-  ];
-  
-  export const testimonials = [
-    {
-      quote: "insert testimonial",
-      name: "Gaurav Hegde",
-      title: "Managing Director of HAMCO Arabia PSD",
-    },
-    {
-      quote: "insert testimonial",
-      name: "Gaurav Hegde",
-      title: "Managing Director of HAMCO Arabia PSD",
-    },
-    {
-      quote: "insert testimonial",
-      name: "Gaurav Hegde",
-      title: "Managing Director of HAMCO Arabia PSD",
-    },
-    {
-      quote: "insert testimonial",
-      name: "Gaurav Hegde",
-      title: "Managing Director of HAMCO Arabia PSD",
-    },
-    {
-      quote: "insert testimonial",
-      name: "Gaurav Hegde",
-      title: "Managing Director of HAMCO Arabia PSD",
-    },
-  ];
-  
-  export const companies = [
-    {
-      id: 1,
-      name: "",
-      img: "/.svg",
-      nameImg: "/.svg",
-    },
-    {
-      id: 2,
-      name: "",
-      img: "/.svg",
-      nameImg: "/.svg",
-    },
-    {
-      id: 3,
-      name: "",
-      img: "/.svg",
-      nameImg: "/.svg",
-    },
-    {
-      id: 4,
-      name: "",
-      img: "/.svg",
-      nameImg: "/.svg",
-    },
-    {
-      id: 5,
-      name: ".",
-      img: "/.svg",
-      nameImg: "/.svg",
-    },
-  ];
-  
-  export const workExperience = [
-    {
-      id: 1,
-      company: "TAMID @ ASU",
-      title: "SWE Consultant",
-      desc: "Developing front-end and back-end systems for various startups and companies through TAMID's Tech Engagements.",
-      className: "md:col-span-2",
-      thumbnail: "/tamid-logo.png",
-    },
-    {
-      id: 2,
-      company: "Arizona State University",
-      title: "Writing Tutor",
-      desc: "Delivering comprehensive writing support to a diverse student body, leveraging strong organizational and communication skills.",
-      className: "md:col-span-2",
-      thumbnail: "/asu-logo.png",
-    },
-    {
-      id: 3,
-      company: "Tutatet",
-      title: "Full-Stack Developer",
-      desc: "Developed and enhanced a responsive and intuitive cross-platform tutoring application using Flutter, connecting students to expert tutors worldwide.",
-      className: "md:col-span-2",
-      thumbnail: "/tutatet-logo.png",
-    },
-    {
-      id: 4,
-      company: "Visionary",
-      title: "Full-Stack Developer",
-      desc: "Building a SaaS to help students consolidate their information using AI, making the college-application process seamless and stress-free. (Coming Soon)",
-      className: "md:col-span-2",
-      thumbnail: "/vzn-logo.png",
-    },
-  ];
-  
-  export const socialMedia = [
-    {
-      id: 1,
-      img: "/git.svg",
-      link: "https://github.com/shettyamoghh"
-    },
-    {
-      id: 3,
-      img: "/link.svg",
-      link: "https://www.linkedin.com/in/shetty-amogh"
-    },
-  ];
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Experience", link: "#experience" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title:
+      "Currently a Junior Computer Science student at Arizona State University",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.png",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "Seamless communication across various time zones",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "making use of",
+    description: "Here is what I am currently",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Passionate about tech, personal finance, and productivity",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+  {
+    id: 5,
+    title:
+      "Learning how to integrate artificial intelligence into web applications",
+    description: "What I'm currently working on",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName:
+      "absolute right-0 bottom-0 md:w-96 w-60 opacity-20 xl:opacity-70",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Let Him Cook AI",
+    des: "React web-application that generates delicious recipes based on your ingredients, powered by Claude's API.",
+    img: "/p2.png",
+    iconLists: ["/re.svg", "/claude.svg", "/html.svg", "/css.svg", "/js.svg"],
+    link: "https://lethimcook-ai.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Tenzies Game",
+    des: "Responsive dice game built with React that's both responsive and fun!",
+    img: "/p3.png",
+    iconLists: ["/re.svg", "/html.svg", "/css.svg", "/js.svg"],
+    link: "https://tenzies-shetty.netlify.app/",
+  },
+  {
+    id: 3,
+    title: "CRUD To-Do List",
+    des: "Keep track of your tasks using this simple to-do list made with React. The tasks never end huh?",
+    img: "/p1.png",
+    iconLists: ["/re.svg", "/html.svg", "/css.svg", "/js.svg"],
+    link: "https://shetty-todolist.netlify.app/",
+  },
+  // {
+  //   id: 4,
+  //   title: "title",
+  //   des: "description",
+  //   img: "/p4.svg",
+  //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+  //   link: "https://github.com/shettyamoghh",
+  // },
+];
+
+export const testimonials = [
+  {
+    quote: "insert testimonial",
+    name: "Gaurav Hegde",
+    title: "Managing Director of HAMCO Arabia PSD",
+  },
+  {
+    quote: "insert testimonial",
+    name: "Gaurav Hegde",
+    title: "Managing Director of HAMCO Arabia PSD",
+  },
+  {
+    quote: "insert testimonial",
+    name: "Gaurav Hegde",
+    title: "Managing Director of HAMCO Arabia PSD",
+  },
+  {
+    quote: "insert testimonial",
+    name: "Gaurav Hegde",
+    title: "Managing Director of HAMCO Arabia PSD",
+  },
+  {
+    quote: "insert testimonial",
+    name: "Gaurav Hegde",
+    title: "Managing Director of HAMCO Arabia PSD",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "",
+    img: "/.svg",
+    nameImg: "/.svg",
+  },
+  {
+    id: 2,
+    name: "",
+    img: "/.svg",
+    nameImg: "/.svg",
+  },
+  {
+    id: 3,
+    name: "",
+    img: "/.svg",
+    nameImg: "/.svg",
+  },
+  {
+    id: 4,
+    name: "",
+    img: "/.svg",
+    nameImg: "/.svg",
+  },
+  {
+    id: 5,
+    name: ".",
+    img: "/.svg",
+    nameImg: "/.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    company: "TAMID @ ASU",
+    title: "SWE Consultant",
+    desc: "Developing front-end and back-end systems for various startups and companies through TAMID's Tech Engagements.",
+    className: "md:col-span-2",
+    thumbnail: "/tamid-logo.png",
+  },
+  {
+    id: 2,
+    company: "Arizona State University",
+    title: "Writing Tutor",
+    desc: "Delivering comprehensive writing support to a diverse student body, leveraging strong organizational and communication skills.",
+    className: "md:col-span-2",
+    thumbnail: "/asu-logo.png",
+  },
+  {
+    id: 3,
+    company: "Tutatet",
+    title: "Full-Stack Developer",
+    desc: "Developed and enhanced a responsive and intuitive cross-platform tutoring application using Flutter, connecting students to expert tutors worldwide.",
+    className: "md:col-span-2",
+    thumbnail: "/tutatet-logo.png",
+  },
+  {
+    id: 4,
+    company: "Visionary",
+    title: "Full-Stack Developer",
+    desc: "Building a SaaS to help students consolidate their information using AI, making the college-application process seamless and stress-free. (Coming Soon)",
+    className: "md:col-span-2",
+    thumbnail: "/vzn-logo.png",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/shettyamoghh",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/shetty-amogh",
+  },
+];
